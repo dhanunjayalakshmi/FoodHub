70624e0f9f5ae4799a6e9d68b3d8f4ef16a311a0
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-05-02 02:16:39 +0530

    Restaurants data is fetched and updated on the page

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,23 +1,41 @@
-const RestaurantCard = () => {
+const RestaurantCard = ({ resData }) => {
+  const {
+    id,
+    name,
+    cloudinaryImageId,
+    areaName,
+    avgRating,
+    costForTwo,
+    cuisines,
+    sla,
+    aggregatedDiscountInfoV3,
+  } = resData;
+
   return (
-    <div className="w-ful flex flex-col rounded-xl">
-      <div class="w-full h-72 overflow-hidden relative rounded-xl">
+    <div className="w-ful flex flex-col rounded-xl transition-transform duration-300 transform hover:scale-95">
+      <div className="w-full h-72 overflow-hidden relative rounded-xl">
         <img
-          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/be187af1-ae9b-46e7-ad51-57cec58fbb2f_256893.jpg"
+          // src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/+${cloudinaryImageId}`}
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
           alt="Food"
-          className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full min-h-full object-cover "
+          className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full min-h-full object-cover"
         />
         <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent"></div>
         <p className="absolute bottom-0 left-0  text-white text-xl font-bold m-1 p-2">
-          20% OFF UPTO 50
+          {aggregatedDiscountInfoV3
+            ? aggregatedDiscountInfoV3?.header +
+              " " +
+              aggregatedDiscountInfoV3?.subHeader
+            : ""}
         </p>
       </div>
-
       <div className="p-2">
-        <p className="px-2 text-xl font-bold">McDonald's</p>
-        <p className="px-2 font-bold">4.2 stars . 45-50 mins</p>
-        <p className="px-2">Burgers, beverages, Cafe, Desserts</p>
-        <p className="px-2">Kondapur</p>
+        <p className="px-2 text-xl font-bold">{name}</p>
+        <p className="px-2 font-bold">
+          {avgRating} stars . {sla.slaString}
+        </p>
+        <p className="px-2">{cuisines.join(", ")}</p>
+        <p className="px-2">{areaName}</p>
       </div>
     </div>
   );
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,30 +1,32 @@
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Restaurants = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchRestaurantsData();
+  }, []);
+
+  const fetchRestaurantsData = async () => {
+    const restaurantsData = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4597641&lng=78.3540402&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await restaurantsData.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
   return (
     <div className="w-full flex flex-col items-center">
       <h2 className="w-[80%] font-bold text-2xl mx-2 my-4 px-1 py-4">
         Restaurants with online food delivery in your area
       </h2>
-      {/* <div className="w-[80%] flex flex-wrap gap-4 justify-evenly mx-auto"> */}
       <div className="w-[80%] grid grid-cols-4 gap-8 mx-auto">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {listOfRestaurants?.map((restaurant) => (
+          <RestaurantCard resData={restaurant?.info} />
+        ))}
       </div>
     </div>
   );
