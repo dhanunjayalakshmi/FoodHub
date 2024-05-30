f47e05beb6b77db68becc9a91dd3ce2c6ea8e66b
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2024-05-30 16:52:08 +0530

    Header is fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const cartItems = useSelector((store) => store?.cart?.items);
+
   return (
     <div className="w-full fixed top-0 flex justify-center py-4 shadow-md z-30 bg-white">
       <div className="flex justify-between items-center w-full max-w-3xl gap-12">
@@ -13,11 +16,24 @@ const Header = () => {
 
         <ul className="flex gap-12 text-bold text-lg">
           {/* <li>Location</li> */}
-          <li className="hover:text-[#f26921]">Search</li>
-          <li className="hover:text-[#f26921]">About Us</li>
-          <li className="hover:text-[#f26921]">Contact Us</li>
-          <li className="hover:text-[#f26921]">Cart</li>
-          <li className="hover:text-[#f26921]">Login</li>
+          <li className="cursor-pointer font-medium text-[#171717] hover:text-[#f26921]">
+            Search
+          </li>
+          <li className="cursor-pointer font-medium text-[#171717] hover:text-[#f26921]">
+            About Us
+          </li>
+          <li className="cursor-pointer font-medium text-[#171717] hover:text-[#f26921]">
+            Contact Us
+          </li>
+          <li className="group cursor-pointer font-medium text-[#171717] hover:text-[#f26921]">
+            Cart{" "}
+            <span className="items-center text-white px-1 bg-green-600 group-hover:bg-[#f26921]">
+              {cartItems?.length}
+            </span>
+          </li>
+          <li className="cursor-pointer font-medium text-[#171717] hover:text-[#f26921]">
+            Login
+          </li>
         </ul>
       </div>
     </div>
